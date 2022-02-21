ffba1ced79bbd4ef54b0ccc2ff23e365cb9a3207
Ahmet Tarık Bostan <a.tarikbostan@gmail.com>
2022-02-21 22:05:14 +0300

    redux-persist

--- e-commerce-simple/src/redux/store.js
+++ e-commerce-simple/src/redux/store.js
@@ -1,12 +1,16 @@
 import { createStore,applyMiddleware } from "redux";
 import logger from "redux-logger";
+import persistStore from "redux-persist/es/persistStore";
 import rootReducer from "./root-reducer";
+
 
 const middlwares = [logger];
 
 const store = createStore(rootReducer,applyMiddleware(...middlwares));
 
-export default store;
+ const persistor = persistStore(store)
+
+export  {store,persistor};
 
 
 
