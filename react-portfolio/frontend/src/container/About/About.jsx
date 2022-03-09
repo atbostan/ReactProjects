99f5a131c364c4cfb4cd4f8b698bd4f1a378d07a
Ahmet Tarık Bostan <a.tarikbostan@gmail.com>
2022-03-09 22:18:53 +0300

    about and header sections completed

--- react-portfolio/frontend/src/container/About/About.jsx
+++ react-portfolio/frontend/src/container/About/About.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import React, { useEffect, useState } from 'react'
 import './About.scss'
+import {images} from '../../constants'
+const abouts = [
+  {title:'Web Development', description:'I am a web developer', imgUrl:images.about01},
+  {title:'Backend Development', description:'I am a web developer', imgUrl:images.about02},
+  {title:'Frontend Development', description:'I am a web developer', imgUrl:images.about03},
+  {title:'UI/UX Design', description:'I am a web developer', imgUrl:images.about04}
+]
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <>
+      <h2 className='app__about head-text'>I Know that <span>Good Development</span><br /> means <span>Good Business</span></h2>
+      <div className='app__profiles'>
+        {abouts.map((about,indx)=>(
+          <motion.div
+            whileInView={{opacity:1}}
+            whileHover={{scale:1.1}}
+            transition={{duration:0.5,type:'tween'}}
+            className='app__profile-item'
+            key={about.title+indx}
+
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className='bold-text' style={{marginTop:20}}>{about.title}</h2>
+            <p className='p-text' style={{marginTop:10}}>{about.description}</p>
+
+
+          </motion.div>
+        ))}
+        </div>
+    </>
+    )
 }
 
 export default About
