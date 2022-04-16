14721d0a1e479fa360db6dd2e5f5ead1d1917879
Ahmet Tarık Bostan <a.tarikbostan@gmail.com>
2022-04-16 21:05:45 +0300

    adding user reducer as a first step of transition to redux

--- clothing-ecommerce-with-redux/src/context/user.context.jsx
+++ clothing-ecommerce-with-redux/src/context/user.context.jsx
@@ -1,5 +1,36 @@
-import { createContext, useState,useEffect } from "react";
+import { createContext, useState,useEffect, useReducer } from "react";
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../utils/firebase.utils";
+
+
+  //-------REDUCER---------
+
+
+  const INITIAL_STATE = {
+    currentUser:null
+  }
+
+  const userReducerAction = {
+    SET_CURRENT_USER:'SET_CURRENT_USER'
+  }
+
+  const userReducer = (state,action)=>{
+    const {type,payload}=action;
+
+    switch (type) {
+      case userReducerAction.SET_CURRENT_USER:
+        return {
+          currentUser:payload
+        }
+        
+       
+    
+      default:
+        throw new Error(`Unexpected action type in user-reducer-${type}`)
+    }
+
+  }
+
+
 
 // as the actual value we want to access from the other any component
 //Context needs initial value
@@ -10,10 +41,17 @@ export const UserContext = createContext({
 
 //Actual component
 export const UserProvider = ({ children }) => {
-
-    //Sets the inital currentUserState
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+  
+    //Hooks for reducer
+      // state->current values been stored
+      // dispatch -> its a function when you call pass the reducer object
+    const [state,dispatch]=useReducer(userReducer,INITIAL_STATE)
+    console.log(state)
+    const {currentUser} = state
+    const setCurrentUser = (user)=>{
+      dispatch({type:userReducerAction.SET_CURRENT_USER,payload:user})
+    }
+    const value = {currentUser,setCurrentUser}
 
   // By doing so this calls only when component mouth
   useEffect(() => {
@@ -27,6 +65,8 @@ export const UserProvider = ({ children }) => {
    return unsubscribe;  //Whenever unmount unsubscribe it
   }, [])
   
+
+
 
   // We can pass the any value inside of the UserContext.Provider by doing so any children component can access these value
   // which this case is the currentUser and SetCurrentUser
