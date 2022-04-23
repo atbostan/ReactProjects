01130871d1943810a6ee19976bfc979199903f67
Ahmet Tarık Bostan <a.tarikbostan@gmail.com>
2022-04-23 23:44:06 +0300

    Redux Dev Tool Integration

--- clothing-ecommerce-with-redux/src/redux/store/store.js
+++ clothing-ecommerce-with-redux/src/redux/store/store.js
@@ -3,28 +3,38 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
-import {persistStore,persistReducer} from "redux-persist"
-import storage from "redux-persist/lib/storage"
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 //Redux Persist -> Tool that allows us the persist the reducer values inside of local storage
 
-
-
 const persistConfig = {
-    key : 'root',
-    storage : storage, //What do we want store this into
-    blacklist :['user'],  //Values which we dont want to persist
+  key: "root",
+  storage: storage, //What do we want store this into
+  blacklist: ["user"], //Values which we dont want to persist
+};
 
-}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const persistedReducer = persistReducer(persistConfig,rootReducer)
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
+//Every store needs reducers in here root reducer
+//Root Reducer => Combination of all reducers
+//We need to rootReducer so that generate st
 
-//Every store needs reducers in here root reducer 
-    //Root Reducer => Combination of all reducers
-                     //We need to rootReducer so that generate store
-const composedEnhancers = compose(applyMiddleware(...middleWares));
-export const store = createStore(persistedReducer,undefined,composedEnhancers)
+const composedEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
 
 export const persistor = persistStore(store);
